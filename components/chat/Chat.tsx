cea30fa91a47912af24511a52254a091b2efbf29
Pranav-Bobde <bobdep31@gmail.com>
2024-05-24 01:00:52 +0530

    fix: :ambulance: removing markdown
    can't pass children as props

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -2,14 +2,10 @@
 
 import { cn } from "@/lib/utils";
 
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import { Avatar } from "@/components/ui/avatar";
 import { Message } from "ai/react";
 import { useEffect, useRef } from "react";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 type ChatProps = {
   className?: string;
@@ -19,6 +15,19 @@ type ChatProps = {
 export default function Chat({ className, messages }: ChatProps) {
   const endReef = useRef<HTMLDivElement>(null);
   const { user, isLoading } = useKindeBrowserClient();
+
+  // const messages={[
+  //   {
+  //     id: "1",
+  //     role: "user",
+  //     content: "Hi!",
+  //   },
+  //   {
+  //     id: "1",
+  //     role: "assistant",
+  //     content: "Hi!",
+  //   },
+  // ]}
 
   useEffect(() => {
     endReef?.current?.scrollIntoView();
@@ -36,30 +45,7 @@ export default function Chat({ className, messages }: ChatProps) {
                 : "You")}
           </Avatar>
           <span className="flex-grow prose dark:prose-invert">
-            <Markdown
-              remarkPlugins={[remarkGfm]}
-              components={{
-                code(props) {
-                  const { children, className, node, ...rest } = props;
-                  const match = /language-(\w+)/.exec(className || "");
-                  return match ? (
-                    <SyntaxHighlighter
-                      {...rest}
-                      PreTag="div"
-                      children={String(children).replace(/\n$/, "")}
-                      language={match[1]}
-                      style={dark}
-                    />
-                  ) : (
-                    <code {...rest} className={className}>
-                      {children}
-                    </code>
-                  );
-                },
-              }}
-            >
-              {message.content}
-            </Markdown>
+            {message.content}
           </span>
         </div>
       ))}
