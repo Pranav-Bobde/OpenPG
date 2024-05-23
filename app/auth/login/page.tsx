df67057291eecfd83f99ac5c5da9895fd97d4a2f
Pranav-Bobde <bobdep31@gmail.com>
2024-05-24 00:36:32 +0530

    refactor: :lipstick: update auth UI

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,52 +1,28 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
-import {
-  LoginLink,
-  RegisterLink,
-} from "@kinde-oss/kinde-auth-nextjs/dist/components";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function page() {
   return (
-    <div className="h-screen h-svh h-dvh w-dvw">
-      <div className="hidden h-full w-full md:grid md:grid-cols-3">
-        <div className="bg-black md:col-span-2 md:flex md:items-center md:justify-center">
-          <div className="text-white text-4xl text-center">OpenPG</div>
-        </div>
+    <div className="h-dvh flex flex-col md:flex-row">
+      <div className="bg-black h-full flex items-end justify-center text-4xl pb-10 md:basis-2/3 md:items-center">
+        <Avatar className="h-28 w-28 md:hidden">
+          <AvatarImage src="/favicon.png" height={50} width={50} />
+          <AvatarFallback>
+            <Skeleton />
+          </AvatarFallback>
+        </Avatar>
 
-        <div className="flex items-center justify-center">
-          <div className="flex-col">
-            <div className="text-3xl text-center p-4">Get Started</div>
-
-            <div className="flex justify-center gap-4">
-              <Button className="w-32">
-                <LoginLink>Login</LoginLink>
-              </Button>
-              <Button className="w-32">
-                <RegisterLink>Register</RegisterLink>
-              </Button>
-            </div>
-          </div>
-        </div>
+        <span className="hidden md:block text-4xl">OpenPG</span>
       </div>
-
-      <div className="h-full flex flex-col justify-center bg-black gap-12 md:hidden">
-        <div className="text-white text-4xl flex justify-center">
-          <Avatar className="h-28 w-28">
-            <AvatarImage src="/favicon.png" height={50} width={50} />
-            <AvatarFallback>
-              <Skeleton />
-            </AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="flex justify-center text-2xl font-semibold">
-          Getting Started
-        </div>
-        <div className="flex flex-col items-center justify-center gap-6">
-          <Button className="w-1/2">
+      <div className="bg-black h-full flex flex-col items-center justify-start gap-4 md:basis-1/3 md:justify-center border-l-2 border-gray-500">
+        <span className="text-2xl mb-4 w-1/2 text-center">Get Started</span>
+        <div className="flex flex-col w-1/2 items-center gap-4 md:flex-row md:w-full md:px-16">
+          <Button className="min-w-fit w-full md:flex-1">
             <LoginLink>Login</LoginLink>
           </Button>
-          <Button className="w-1/2">
+          <Button className="min-w-fit w-full md:flex-1 ">
             <RegisterLink>Register</RegisterLink>
           </Button>
         </div>
